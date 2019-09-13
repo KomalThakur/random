53177e00695a598647bee100cb5476c70cca1c43
Oracle Public Cloud User <komalthakur2608@gmail.com>
2019-09-13 14:27:35 +0000

    abc

--- app/config/oicConfig.js
+++ app/config/oicConfig.js
@@ -1,12 +1,12 @@
 module.exports = {
   requestUrlPO:
-    "https://OIC-GE-BC-gecorporatepoc.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/PO_CAPTUREB_SEND_EBS/1.0/poevents",
+   "https://vbcs-bc-sc-oabcs1.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/PO_CAPTUREB_SEND_EBS/1.0/poevents", 
   requestUrlInvoice:
-    "https://OIC-GE-BC-gecorporatepoc.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/SUBMITINVOICE_BC/1.0/invoice",
-    requestUrlDisputeInvoice : "https://OIC-GE-BC-gecorporatepoc.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/UPDATEINVOICEFORDISPUTE/1.0/updateDisputeInvoice",
-    requestUrlSettlement: "https://OIC-GE-BC-gecorporatepoc.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/CAPTURE_SETTELME_EVENTS_BC/1.0/getSettlementEvent",
+    "https://vbcs-bc-sc-oabcs1.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/SUBMITINVOICE_BC/1.0/invoice",
+    requestUrlDisputeInvoice : "https://vbcs-bc-sc-oabcs1.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/UPDATEINVOICEFORDISPUTE/1.0/updateDisputeInvoice",
+    requestUrlSettlement: "https://vbcs-bc-sc-oabcs1.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/CAPTURE_SETTELME_EVENTS_BC/1.0/getSettlementEvent",
     headers: {
-    Authorization: "Basic YXBwZGV2dGVzdDpHZXRlc3QxMjM0NTY=",
+    Authorization: "Basic aWNsZWRnZXI6V2VsY29tZSMxMjM0NQ==",
     "Content-Type": "application/json"
   }
 };
